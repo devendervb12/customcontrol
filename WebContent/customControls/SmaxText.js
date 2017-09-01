f0b11f44472374ee1f46a71385e0a533f318bc2b
devendervb12 <info@s-max.in>
2017-09-01 07:30:46 +0530

    intial coomit

---
+++ WebContent/customControls/SmaxText.js
@@ -0,0 +1,13 @@
+
+sap.ui.core.Control.extend("smax.ui.Text", {
+	metadata : {
+		properties : {
+			text : "String" // framework read this line, framework will create
+				// setter and getter method
+		}
+	},
+	renderer : function(oRm, oControl){
+		oRm.write("<p>"+oControl.getText()+"</p>")
+	}
+});
+
